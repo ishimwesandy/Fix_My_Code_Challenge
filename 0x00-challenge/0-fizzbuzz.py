3ab0966a5b3f760467bcbad74054a8ff547565e8
Sandrine Ishimwe <sandrineishimwe12@gmail.com>
2023-10-28 12:43:55 +0300

    changing file code

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" FizzBuzz
-"""
+""" FizzBuzz"""
+
+
 import sys
 
 
 def fizzbuzz(n):
     """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-
+   FizzBuzz function prints numbers from 1 to n separated by a space.
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
@@ -19,7 +19,7 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        if (i % 3) == 0:
+        elif (i % 3) == 0:
             tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 15")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
     number = int(sys.argv[1])
